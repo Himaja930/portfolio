37521887c7ddeda0830214760a23677e0f27ae1f
Himaja <84573936+Himaja930@users.noreply.github.com>
2024-01-05 18:05:33 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let hamberger = document.querySelector('.container-hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+let links= document.querySelector('.list');
+
+console.log('coming');
+
+hamberger.addEventListener('click',function(){
+    console.log('coming');
+        mobileNav.classList.add('open');
+})
+times.addEventListener('click',function(){
+        mobileNav.classList.remove('open');
+})
+links.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+})
+
+
